ad46cff2ae38f31eb6be2234f2bddcf85d64faec
Lilit <lilit95v@gmail.com>
2021-04-16 17:14:02 +0400

    created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BlocksComponent } from './blocks/blocks.component';
+import { BlockComponent } from './block/block.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BlocksComponent,
+    BlockComponent
+    
   ],
   imports: [
     BrowserModule,
